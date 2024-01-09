3a5ee51f3722bcf21e812141e0754c8797d657dc
CR1SS19 <cristhiancaua@gmail.com>
2024-01-09 16:15:06 -0300

    improvements to the zonetransfer.sh code

--- zonetransfer.sh
+++ zonetransfer.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-# Use ./zonetransfer.sh example.com
+# Usage: ./zonetransfer.sh <domain_name>
 
-for server in $(host -t ns $1 | cut -d " " -f 4);
-do
-host -l -a $1 $server
+# Check for required argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <domain_name>"
+  exit 1
+fi
+
+# Get nameservers for the domain
+nameservers=$(host -t ns "$1" | cut -d " " -f 4)
+
+# Iterate through nameservers and perform zone transfers
+for server in $nameservers; do
+  echo "Attempting zone transfer from $server..."
+  host -l -a "$1" "$server"
+
+  # Check for errors in zone transfer
+  if [ $? -ne 0 ]; then
+    echo "Zone transfer from $server failed."
+  else
+    echo "Zone transfer from $server successful."
+  fi
 done
