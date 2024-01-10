6b72d240871f1851a79af4f069de984ca12de8ea
Cristhian Cauã Jesus de Oliveira <cristhian.oliveira@yssy.com.br>
2024-01-10 14:42:17 -0300

    add metadata parser script, NOT YET TESTED

--- Information Gathering/metadata_analyzer.sh
+++ Information Gathering/metadata_analyzer.sh
@@ -4,7 +4,19 @@
 
 # Check for required arguments
 if [ $# -ne 2 ]; then
-  echo "Usage: $0 <domain_name> <file_extension>"
-  exit 1
+    echo "=========================================================="
+    echo " Support pdf, doc, docx, xls, xlsx, ppt, pptx"
+    echo "=========================================================="
+    echo " Usage: $0 <domain_name> <file_extension>"
+    echo "=========================================================="
+    exit 1
 fi
+ 
+lynx --dump "https://google.com/search?&q=site:$1+ext:$2" | grep ".$2" | cut -d "=" -f2 | egrep -v "site|google" | sed 's/...$//' > $2.txt
 
+for url in $(cat $2.txt); do
+    wget -q $url;
+done
+
+ls *.$2
+exiftool *.$2 
