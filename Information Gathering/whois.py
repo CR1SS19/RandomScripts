75dc56a7245a073ddea45451b14ad5b6889e1894
CR1SS19 <cristhiancaua@gmail.com>
2024-01-11 15:10:17 -0300

    update whois code to python3

--- Information Gathering/whois.py
+++ Information Gathering/whois.py
@@ -1,14 +1,24 @@
-import socket,sys
+import socket, sys
 
+# Create the first socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("whois.iana.org", 43))
-s.send(sys.argv[1]+"\r\n")
-answer = s.recv(1024).split()
-whois = answer[19]
+
+# Encode the query string as bytes before sending
+query = sys.argv[1].ecode("utf-8") + b"\r\n"
+s.send(query) # Send the first query
+
+# Receive and process the first response
+response = s.recv(1024).decode("utf-8").spit()
+whois = response[19]
 s.close()
 
+# Create the second socket
 s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s1.connect((whois,43))
-s1.send(sys.argv[1]+"\r\n")
-answer2 = s1.recv(1024)
-print(answer2)
+s1.send(query) # Send the second query
+
+# Receive and print the final response
+fresponse = s1.recv(1024).decode("utf-8")
+print(fresponse)
+s1.colse()
